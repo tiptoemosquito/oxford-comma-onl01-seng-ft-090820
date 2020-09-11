7abbd08e2a8c0087e8a96abcd9de9f8d0a9d6b5f
Christina Howard <clhoward33@icloud.com>
2020-09-11 17:42:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,6 @@
 def oxford_comma(array)
-  array = []
-  if 1-element.split do
-  elsif 2-element.split do
-  elsif 3-element.split do
-  elsif 4-element.split do
-  else 5-element.split do
-  end
-  end
-  end
-end
+
+  
 
 
 end
