5766cef70fb867fb101e5e22dba7d5f47e49207f
Christina Howard <clhoward33@icloud.com>
2020-09-11 20:10:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-array = ["1-element", "2-element", "3-element"]
-array.split
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
