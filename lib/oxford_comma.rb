c59418e0a8647fba0e96fa88ef2b995344faeccd
Christina Howard <clhoward33@icloud.com>
2020-09-11 17:40:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
-  array = ["kiwi, durian, starfruit, mangos, and dragon fruits"]
+  array = []
+  if 1-element.split do
+  elsif 2-element.split do
+  elsif 3-element.split do
+  elsif 4-element.split do
+  else 5-element.split do
+  end
+  end
+  end
+end
 
 
 end
